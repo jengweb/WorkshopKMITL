94aa8bd8e529946e9296af0764041a308bda98ff
Phichitchai Udorntak <phichitchai@Phichitchais-MacBook-Pro.local>
2017-10-08 15:44:07 +0700

    group10

--- group10/app.py
+++ group10/app.py
@@ -1,26 +1,15 @@
-from flask import Flask, render_template, request
-import csv
-
+from flask import Flask,render_template ,request
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-   return render_template('hello.html')
+def hello_world():
+    return render_template('instascan-master/docs/index.html')
 
-@app.route('/insert_profile', methods=['POST'])
-def insert_profile():
-    name = request.form['name']
+@app.route('/result' , methods = ['POST'])
+def result():
+    firstname = request.form['firstname']
     lastname = request.form['lastname']
-    with open('names.csv', 'a') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([name, lastname])
-
-    with open('names.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
-        return render_template('result.html', reader=reader)
-
-# def FizzBuzz(number):
-#     return number
+    return render_template('vs2.html', firstname = firstname, lastname = lastname)
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", debug=True)
+    app.run(host = "0.0.0.0" , debug = True, use_reloader=False)
